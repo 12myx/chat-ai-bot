ccf2f936c617e65516d4cf9bbf3b6ed586181588
12myx <1489007907@qq.com>
2026-05-27 21:54:25 +0800

    feat:多轮上下文记忆

--- web_version/main.py
+++ web_version/main.py
@@ -18,7 +18,12 @@ client = OpenAI(
     api_key=api_key,
     base_url="https://api.deepseek.com"
 )
-
+messages = [
+    {
+        "role": "system",
+        "content": "你是一个AI助手"
+    }
+]
 # 打开网页
 @app.get("/")
 def home():
@@ -32,30 +37,34 @@ class ChatRequest(BaseModel):
 # AI聊天接口
 @app.post("/chat")
 def chat(request: ChatRequest):
+    # 构建消息列表
+    messages.append({
+        "role": "user",
+        "content": request.message
+    })
 
     response = client.chat.completions.create(
         model="deepseek-chat",
-        messages=[
-            {
-                "role": "system",
-                "content": "你是一个AI助手"
-            },
-            {
-                "role": "user",
-                "content": request.message
-            }
-        ],
-        #流式输出
+        messages=messages,
         stream=True
     )
-# 流式响应（核心为循环）
+
+    # 流式响应（核心为循环）
     def generate():
+
+        ai_reply = ""
 
         for chunk in response:
 
             content = chunk.choices[0].delta.content
 
             if content:
+
+                ai_reply += content
+
                 yield content
 
-    return StreamingResponse(generate(), media_type="text/plain")
+        messages.append({
+            "role": "assistant",
+            "content": ai_reply
+        })
